3f9c52c298a6955245d6485b9ddd797243a978c6
Tyler <Tyler.R.Moore@outlook.com>
2021-12-05 05:52:54 -0500

    Section 2: Flow Control (If, Else, Elif, While, For)

---
+++ if_else_example.py
@@ -0,0 +1,7 @@
+print('Please type your password below.')
+myPass = input()
+# password = 'swordfish'
+if myPass == 'swordfish':
+    print('Access granted!')
+else:
+    print('Access not granted!')
---
+++ if_example.py
@@ -0,0 +1,5 @@
+print('Hello, may I ask your name?')
+myName = input()
+if myName == 'Alice' or myName == 'alice': # Incorporates an or statement within an IF statement
+    print('Hi Alice')
+print('Done')
