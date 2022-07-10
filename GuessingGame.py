61550399cfdfea06926600ddd81317a4a1f9c189
Tyler Moore <Tyler.R.Moore@outlook.com>
2022-07-10 06:38:26 -0400

    Adding/updating files from previous sections of ATBS

--- GuessingGame.py
+++ GuessingGame.py
@@ -7,17 +7,17 @@ name = input()
 attempts = int(6)
 print('\n', name + '? That is a good name!', sep='\n', end=' ')
 print('How about we play a game? You win, you get to leave, you lose you stay forever.\n\n')
-print('What number I am thinking of between 1 and 30? Be careful, you only have ' + str(attempts) + ' guesses!') # made "attempts" variable to print 6 prior to range being declared in part 2
-Jackpot = random.randint(1, 30)
+print('What number I am thinking of between 1 and 100? Be careful, you only have ' + str(attempts) + ' guesses!') # made "attempts" variable to print 6 prior to range being declared in part 2
+Jackpot = random.randint(1, 100)
 
 
 #part 2: Win, lose, repeat!
 for Numguess in range (1, 7): # allows 6 guesses
-    try :
+    try:
         guess = int(input())
-        if guess < Jackpot :
+        if guess < Jackpot:
             print('You are too low, try again.')
-        elif guess > Jackpot :
+        elif guess > Jackpot:
             print('You are too high, try again.')
         else:
             break # means they got it correct
@@ -26,13 +26,18 @@ for Numguess in range (1, 7): # allows 6 guesses
         print('While that may be a number, make it easier on yourself and use the numpad, TRY AGAIN!')
 
 #part 3: The win, the guess, the escape.
-if guess == Jackpot :
+if guess == Jackpot:
     print('Wow, you must be a mind reader, kind of freaky if you ask me.')
     print('A deal is a deal, you won our little game, so with that you may leave.')
-    print('\n\n\n\n------------------- After credits stats ------------------- ')
+    print('\n\n\n------------------- After credits stats ------------------- ')
     print('You took ' + str(Numguess) + ' guesses, try again to beat your previous score!') # shows how to take the Numguess in the for loop in part 2 and print it
+    print('\n\nPress any key to exit')
+    bye =  input()
 else:
     print('\n\nYou exceeded the amount of ' + str(Numguess) + ' guesses and failed to escape, play again to save yourself.', end=' ')
     print('Oh, by the way, the number that I was thinking of was ' + str(int(Jackpot)) + '.')
+    print('\n\nYou lost, you are trapped with me forever!!')
+    byeagain = input()
     
-    
+
+
